7c7044d5deaee0993d8b232ae8dca83ebdaa418a
MISBAH <misbah@MacBook-Pro-1.local>
2019-07-29 15:14:21 +0700

    Initial commit

---
+++ app/src/main/java/gntng/wafi/com/kelas8d/MainActivity.java
@@ -0,0 +1,30 @@
+package gntng.wafi.com.kelas8d;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listDelapan);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, " POSISI YANG DITEKAN = " + position, Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this,DetailActivity.class)
+                        .putExtra("KEY_ID", position));
+            }
+        });
+    }
+}
